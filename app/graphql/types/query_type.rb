e31aea3857cc8d9965a9bf16c4513730467b102a
togo-mentor <uint2435@gmail.com>
2023-02-12 09:09:19 +0900

    feat: create book query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,6 +5,7 @@ module Types
     include GraphQL::Types::Relay::HasNodesField
 
     field :books, resolver: Queries::Books
+    field :book, resolver: Queries::Book
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
   end
